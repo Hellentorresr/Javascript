1c7bf55d2d747835c47bbb84c3d1404681ff4790
Hellentorresr <htorresr1@ucenfotec.ac.cr>
2022-05-10 16:00:06 -0600

    Hellen

---
+++ index.js
@@ -0,0 +1,25 @@
+let arbol = "arbol con grandes hojas verdes y puede ser grandes y pequenos";
+
+console.log(arbol.split(''));
+
+//codigo html
+let codigoHtml = `
+    <li>hola uno</li>
+    <li>hola dos</li>
+    <li>hola tres</li>`;
+
+    console.log(codigoHtml);
+
+let number = new Number(5);
+console.log(number)
+
+console.log(Boolean(0));
+console.log(Boolean(-7));
+console.log(Boolean(''));
+console.log(Boolean(' '));
+
+let casa;
+console.log(casa);
+
+let probarSiesUnFNumero = 'hola'*3.7
+console.log(probarSiesUnFNumero);
