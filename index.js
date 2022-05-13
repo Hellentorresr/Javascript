33205b71d49bf1d6ded16c6cf05d5b5062b043d7
Hellentorresr <htorresr1@ucenfotec.ac.cr>
2022-05-13 15:28:39 -0600

    updating Function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let arbol = "arbol con grandes hojas verdes y puede ser grandes y pequenos";
 
-console.log(arbol.split(''));
+/* console.log(arbol.split('')); */
 
 //codigo html
 let codigoHtml = `
@@ -8,7 +8,7 @@ let codigoHtml = `
     <li>hola dos</li>
     <li>hola tres</li>`;
 
-    console.log(codigoHtml);
+/* console.log(codigoHtml);
 
 let number = new Number(5);
 console.log(number)
@@ -16,10 +16,42 @@ console.log(number)
 console.log(Boolean(0));
 console.log(Boolean(-7));
 console.log(Boolean(''));
-console.log(Boolean(' '));
+console.log(Boolean(' ')); */
 
 let casa;
 console.log(casa);
 
-let probarSiesUnFNumero = 'hola'*3.7
+let probarSiesUnFNumero = 'hola' * 3.7
 console.log(probarSiesUnFNumero);
+
+
+//Fuctions declaration
+function thisIsAFunction() {
+    console.log('hi');
+}
+
+
+//calling the function
+thisIsAFunction();
+
+//function returning a value
+function aFunctionRetorning() {
+    // A function can return diferents data type
+    return 'The function has return a string'
+}
+
+let printTheFunction = aFunctionRetorning();
+console.log(printTheFunction);
+
+
+//function with Parameters
+function doIt(nomberOne, nomberTwo) {
+    let mult = nomberOne * nomberTwo;
+    let div = nomberOne / nomberOne;
+
+    return `The multiplication is: ${mult} and the divition is: ${div}`
+}
+
+let Operations = doIt(5, 5);
+
+console.log(Operations);
